6ee935ecad9b6cd46481a4d7dc2bce014dab419d
TheTabular <thetabular@gmail.com>
2025-08-20 11:16:47 -0500

    Updated

--- src/app/components/content/PythonContent.tsx
+++ src/app/components/content/PythonContent.tsx
@@ -35,7 +35,7 @@ export default function PythonContent({ onBackChange }: { onBackChange?: (showBa
         <div>
           <div className="flex items-center mb-4">
             <div className="flex-grow h-px bg-black"></div>
-            <h3 className="text-xl font-semibold text-gray-900 px-4">Data Manipulation</h3>
+            <h3 className="text-xl font-semibold text-gray-900 px-4">Data Cleaning</h3>
             <div className="flex-grow h-px bg-black"></div>
           </div>
           <div className="flex justify-center px-4">
